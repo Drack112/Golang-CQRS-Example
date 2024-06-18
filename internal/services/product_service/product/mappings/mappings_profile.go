f15a240f0aab727794542c4c6f769917b978aaa5
João Vitor <61944386+Drack112@users.noreply.github.com>
2024-06-18 11:55:16 -0300

    [FEAT] - CreatedMap for ProductCreated

--- internal/services/product_service/product/mappings/mappings_profile.go
+++ internal/services/product_service/product/mappings/mappings_profile.go
@@ -3,11 +3,17 @@ package mappings
 import (
 	"github.com/Drack112/Golang-GQRS-Example/internal/pkg/mapper"
 	"github.com/Drack112/Golang-GQRS-Example/internal/services/product_service/product/dtos"
+	"github.com/Drack112/Golang-GQRS-Example/internal/services/product_service/product/features/creating_product/v1/events"
 	"github.com/Drack112/Golang-GQRS-Example/internal/services/product_service/product/models"
 )
 
 func ConfigureMappings() error {
 	err := mapper.CreateMap[*models.Product, *dtos.ProductDto]()
+	if err != nil {
+		return err
+	}
+
+	err = mapper.CreateMap[*models.Product, *events.ProductCreated]()
 	if err != nil {
 		return err
 	}
